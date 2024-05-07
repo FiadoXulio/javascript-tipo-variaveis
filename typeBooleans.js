4ce5f33a43c26028b877d7d9c7313ac7d02907e2
FiadoXulio <giovanna.teodoro.santos@escola.pr.gov.br>
2024-05-07 09:56:00 -0300

    Feat add Tipos booleanos

---
+++ typeBooleans.js
@@ -0,0 +1,17 @@
+//Boolean
+//Verdadeiro ou falso
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+// === Compara tipo e valor
+console.log(primeiroNumero === segundoNumero);
+
+// Declarar uma variavel com um valor booleano
+const ativo = falso;
+let ativo2 = true;// vai permitir a mudança do valor da variavel
+
+const nome = "Giovanna";
+const sobrenome = "Silva";
+
+console.log(nome === sobrenome);
