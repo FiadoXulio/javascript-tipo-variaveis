f10a2e7211acf8b4d84fec3e1cc1bacf5a7251df
FiadoXulio <giovanna.teodoro.santos@escola.pr.gov.br>
2024-05-14 09:31:26 -0300

    Feat add null

---
+++ tiposEspeciaisNullUndefined.js
@@ -0,0 +1,12 @@
+// Null
+// Ausencia de valor
+// Atribuimos o null a uma variavel
+
+const valorNulo = null;
+
+// Underined
+// Ausencia de valor
+// Mas um tipo diferente de Ausencia de valor
+// Nao permite o uso de const pois variaveis const nessecitam
+// ser inicializaa
+var valorUndefined;
